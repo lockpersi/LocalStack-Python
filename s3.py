ae7a2987dca57b058b1a4fe24f86b652faa0d924
lockpersi <guuhs@me.com>
2025-05-17 01:59:56 +0000

    inicial

---
+++ s3.py
@@ -0,0 +1,22 @@
+import boto3
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url="http://localhost:4566",
+    aws_access_key_id="test",
+    aws_secret_access_key="test",
+    region_name="us-east-1"
+)
+
+bucket_name = "meu-bucket-local"
+file_name = "exemplo.txt"
+
+s3.create_bucket(Bucket=bucket_name)
+with open(file_name, "w") as f:
+    f.write("Conteúdo de exemplo")
+
+s3.upload_file(file_name, bucket_name, file_name)
+
+print("Objetos no bucket:")
+for obj in s3.list_objects(Bucket=bucket_name).get("Contents", []):
+    print(" -", obj["Key"])
